e564f75dd6191825f58cc31d286d9e4021f00809
okaryo <okaryotr@gmail.com>
2019-12-15 17:08:12 +0900

    [add] merge todo component

--- plain-react/src/App.tsx
+++ plain-react/src/App.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header'
+import InputForm from './components/InputForm'
+import Todos from './components/Todos'
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="header">TODO LIST</header>
-    </div>
-  );
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      todos: [{name: '昼飯食べる'}, {name: '晩飯食べる'}]
+    }
+  }
+  setTodo = (e: any) => {
+    this.setState({
+        todos: this.state.todos.concat({name: e})
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header styleName={ "header" } />
+        <InputForm
+          dataState={(e: any) => this.setTodo(e)}
+        />
+        <Todos
+          todos={ this.state.todos }
+        />
+        {this.state.todos.map((todo: any) => (
+          <Todos key={todo.name} todoName={todo.name} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
